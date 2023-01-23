352c83d3f901dbb58bd7ad1517151c610c0f2fe3
laoshibajiao <1010404648@qq.com>
2023-01-23 17:59:21 +0800

    完整简单的web项目

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,14 +2,22 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Hello world!
  *
  */
 @EnableAutoConfiguration
+//实现 MVC 的功能
+@RestController
 public class App
 {
+    @RequestMapping("/")
+    public String home(){
+        return "hello world";
+    }
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
